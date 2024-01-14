75b27da5fe2ad238934d48c2a8c4194897cbc18c
Iliand-DC <iluha805@gmail.com>
2024-01-14 11:34:40 +0300

    Переписал для работы с n уравнениями

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,70 @@
 fn main() {
-    let mut t: Vec<i32> = Vec::new();
+    let mut t: Vec<f64> = Vec::new();
     for i in 0..100 {
-        t.push(i);
+        t.push(f64::from(i));
     }
-    let start = 0.0;
+    let mut start = Vec::new();
+    start.push(0.0);
+    start.push(0.0);
     let result = rk4(start, t, dxdt);
     for i in result {
-        print!("{} ", i);
+        for j in i {
+            print!("{} ", j);
+        }
     }
 }
 
 #[allow(unused_variables)]
-fn dxdt(x: f64, t: f64) -> f64 {
-    t*t
+fn dxdt(x: Vec<f64>, t: f64) -> Vec<f64> {
+    vec![t*t, t+t]
 }
 
-fn rk4(start: f64, t: Vec<i32>, function: fn(f64, f64) -> f64) -> Vec<f64> {
+fn rk4(mut value: Vec<f64>, t: Vec<f64>, function: fn(Vec<f64>, f64) -> Vec<f64>) -> Vec<Vec<f64>> {
     let h: f64 = f64::from(t[1] - t[0]);
+    let value_1 = value.clone();
 
-    let mut result: Vec<f64> = Vec::new();
-    result.push(start);
+    let mut result: Vec<Vec<f64>> = Vec::new();
+    result.push(value.clone());
 
     for i in 0..t.len() {
-        let k1 = function(result[i], f64::from(t[i]));
-        let k2 = function((result[i] + h/2.0)*k1, f64::from(t[i])+h/2.0);
-        let k3 = function((result[i] + h/2.0)*k2, f64::from(t[i])+h/2.0);
-        let k4 = function((result[i] + h)*k3, f64::from(t[i])+h);
 
-        result.push(result[i] + h/6.0*(k1 + 2.0*k2 + 2.0*k3 + k4));
+        let mut k1:Vec<f64> = Vec::new();
+        let mut value_2: Vec<f64> = Vec::new();
+        
+        for j in 0..value_1.len() {
+            k1.push(function(value.clone(), t[i])[j] * h);
+            value_2.push(value[j] + k1[j]/2.0);
+        }
+
+        let mut k2: Vec<f64> = Vec::new();
+        let mut value_3: Vec<f64> = Vec::new();
+        for j in 0..value_1.len() {
+            k2.push(function(value_2.clone(), t[i]+h/2.0)[j] * h);
+            value_3.push(value[j] + k2[j]/2.0);
+        }
+       
+
+        let mut k3:Vec<f64> = Vec::new();
+        let mut value_4:Vec<f64> = Vec::new();
+        for j in 0..value_1.len() {
+            k3.push(function(value_3.clone(), t[i]+h/2.0)[j] * h);
+            value_4.push(value[j] + k3[j]);
+        }
+        
+        let mut k4:Vec<f64> = Vec::new();
+        for j in 0..value_1.len() {
+            k4.push(function(value_4.clone(), t[i]+h)[j] * h);
+        }
+
+        let mut delta_value: Vec<f64> = Vec::new();
+        for j in 0..value_1.len() {
+            delta_value.push((k1[j] + 2.0*k2[j] + 2.0*k3[j] + k4[j])/6.0);
+        }
+
+        for j in 0..value_1.len() {
+            value[j] = value[j] + delta_value[j];
+        }
+        result.push(value.clone());
     }
     result
 }
